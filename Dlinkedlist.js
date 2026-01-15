09613ea44f34bd7be951ee2a9c576420f63ddf6e
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-01-15 20:14:51 +0530

    done doubleLinkedlist

--- Dlinkedlist.js
+++ Dlinkedlist.js
@@ -41,8 +41,106 @@ class DoubleLL {
     }
   }
 
-  
+  traverseFromStart() {
+    if (this.head === null) {
+      return;
+    }
+
+    let current = this.head;
+    do {
+      console.log(current.data);
+      current = current.next;
+    } while (current !== null);
+  }
+
+  traverseFromEnd() {
+    if (this.tail === null) return;
+
+    let current = this.tail;
+    do {
+      console.log(current.data);
+      current = current.previous;
+    } while (current !== null);
+  }
+
+ deleteValue(value) {
+  if (!this.head) return;
+
+  let current = this.head;
+
+  while (current) {
+    if (current.data === value) {
+
+      // head
+      if (current === this.head) {
+        this.head = current.next;
+        if (this.head) this.head.previous = null;
+      }
+
+      // tail
+      else if (current === this.tail) {
+        this.tail = current.previous;
+        if (this.tail) this.tail.next = null;
+      }
+
+      // middle
+      else {
+        current.previous.next = current.next;
+        current.next.previous = current.previous;
+      }
+
+      return; // ✅ stop after delete
+    }
+    current = current.next;
+  }
 }
 
 
-let ab = new DoubleLL()
+  search(value) {
+    if (this.head === null) {
+      return;
+    }
+
+    let current = this.head;
+
+    while (current !== null) {
+      if (current.data === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  count() {
+    if (this.head === null) return 0;
+    let current = this.head;
+
+    let count = 0;
+    while (current !== null) {
+      current = current.next;
+      count++;
+    }
+    return count;
+  }
+}
+
+let ab = new DoubleLL();
+
+ab.addLast(10);
+ab.addLast(20);
+ab.addLast(30);
+ab.addLast(50);
+ab.addLast(70);
+ab.addLast(60);
+ab.addFirst(4);
+
+ab.traverseFromStart();
+
+console.log("value", ab.search(70));
+ ab.deleteValue(70)
+console.log("count", ab.count());
+
+ab.traverseFromStart()
+
+ab.count();
